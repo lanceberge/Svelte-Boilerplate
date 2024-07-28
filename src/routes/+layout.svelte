524853b6d97bfb9d22e6a82eac303db2d8e6eafc
lanceberge <bergeron.lance6@gmail.com>
2024-07-28 12:15:55 -0400

    color classes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,11 @@
   import "../app.css";
 </script>
 
-<nav class="bg-gray-200 w-full">
+<nav class="bg-main text-white w-full">
   <div class="container mx-auto px-4 py-2 flex justify-between items-center">
     <div>
-      <a href="/" class="text-gray-800 hover:text-gray-600 mx-2">home</a>
-      <a href="/blog" class="text-gray-800 hover:text-gray-600 mx-2">blog</a>
+      <a href="/" class="hover:text-gray-600 mx-2">home</a>
+      <a href="/blog" class="hover:text-gray-600 mx-2">blog</a>
     </div>
   </div>
 </nav>
