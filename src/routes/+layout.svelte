e523e807b5e94c055406812454b1e9448f8c4c8f
lanceberge <bergeron.lance6@gmail.com>
2024-07-29 19:59:51 -0400

    setup for supabase authentication

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
   import "../app.css";
+  import { invalidate } from '$app/navigation';
+  import { onMount } from 'svelte';
+
+  export let data;
+  $: ({ session, supabase } = data);
+
+  // client-side auth listener
+  onMount(() => {
+    const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+      if (newSession?.expires_at !== session?.expires_at) {
+        invalidate('supabase:auth');
+      }
+    });
+
+    return () => data.subscription.unsubscribe();
+  });
 </script>
 
 <nav class="bg-main text-white w-full">
